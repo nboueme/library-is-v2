2dfa2d172a4cb8756a33e5a33f155daf5248ebec
Nicolas Bouème <nicolasboueme@gmail.com>
2018-05-24 00:25:48 +0200

    #1 web-service: findAllByWorkId and findAllByUserId ORDER BY reservationDate ASC

--- web-service/library-repository/src/main/java/com/nb/library/repository/impl/data/ReservationRepository.java
+++ web-service/library-repository/src/main/java/com/nb/library/repository/impl/data/ReservationRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.repository.query.Param;
 
 public interface ReservationRepository extends CrudRepository<Reservation, Integer> {
 
-    @Query("SELECT r FROM reservation r JOIN FETCH r.work w JOIN FETCH r.user u WHERE w.id = :work_id")
+    @Query("SELECT r FROM reservation r JOIN FETCH r.work w JOIN FETCH r.user u WHERE w.id = :work_id ORDER BY r.reservationDate ASC")
     Iterable<Reservation> findAllByWorkId(@Param("work_id") Integer workId);
 
-    @Query("SELECT r FROM reservation r JOIN FETCH r.work w JOIN FETCH r.user u WHERE u.id = :user_id")
+    @Query("SELECT r FROM reservation r JOIN FETCH r.work w JOIN FETCH r.user u WHERE u.id = :user_id ORDER BY r.reservationDate ASC")
     Iterable<Reservation> findAllByUserId(@Param("user_id") Integer userId);
 }
